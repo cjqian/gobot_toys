b482fae6b912bbbf6c3592ec3699e65631e23317
Crystal Qian <crystal_qian@cable.comcast.net>
2015-07-15 15:08:34 -0600

    Less flickering

--- temp_display.go
+++ temp_display.go
@@ -12,17 +12,23 @@ import (
 	"time"
 )
 
+var (
+	min  float64
+	max  float64
+	diff float64
+)
+
 //assume minimum is 60, max is 80
 func getRed(ftemp float64) int {
 	//range from 0 to 20 now
-	ftempConv := ftemp - 60
-	red := (ftempConv / 20) * 255
+	ftempConv := ftemp - min
+	red := (ftempConv / diff) * 255
 	return int(red)
 }
 
 func getBlue(ftemp float64) int {
-	ftempConv := ftemp - 60
-	blue := (1 - (ftempConv / 20)) * 255
+	ftempConv := ftemp - min
+	blue := (1 - (ftempConv / diff)) * 255
 	return int(blue)
 }
 
@@ -31,24 +37,82 @@ func main() {
 
 	board := edison.NewEdisonAdaptor("board")
 	screen := i2c.NewGroveLcdDriver(board, "screen")
+	buzzer := gpio.NewBuzzerDriver(board, "buzzer", "3")
 	sensor := gpio.NewGroveTemperatureSensorDriver(board, "sensor", "1")
 
+	//the hot song is
 	work := func() {
-		gobot.Every(time.Second*2, func() {
+		//initial calibration in fahrenheit
+		min = 60.0
+		max = 80.0
+		diff = max - min
+		type note struct {
+			tone     float64
+			duration float64
+		}
+		//the cold song is "do you want to build a snowman"
+		coldSong := []note{
+			{gpio.C3, gpio.Quarter},
+			{gpio.C3, gpio.Quarter},
+			{gpio.C3, gpio.Quarter},
+			{gpio.G3, gpio.Quarter},
+			{gpio.C3, gpio.Quarter},
+			{gpio.E4, gpio.Quarter},
+			{gpio.D4, gpio.Half},
+			{gpio.E4, gpio.Half},
+		}
+		//the hot song is "let's get it started in here"
+		hotSong := []note{
+			{gpio.E4, gpio.Quarter},
+			{gpio.E4, gpio.Quarter},
+			{gpio.E4, gpio.Quarter},
+			{gpio.E4, gpio.Quarter},
+			{gpio.C3, gpio.Quarter},
+			{gpio.A3, gpio.Quarter},
+			{gpio.C3, gpio.Quarter},
+		}
+		gobot.Every(time.Second, func() {
 			screen.Clear()
+			time.Sleep(5 * time.Millisecond)
 			screen.Home()
+			time.Sleep(5 * time.Millisecond)
+
+			//get temps
 			celsius := sensor.Temperature()
 			fahrenheit := (celsius * 1.8) + 32
-			screen.Write(fmt.Sprintf("%.2fF, %.2fC", fahrenheit, celsius))
+			//recalibrate
+			min = math.Min(fahrenheit, min)
+			max = math.Max(fahrenheit, max)
+			diff = max - min
+
+			//check
+			if fahrenheit > 80 {
+				for _, val := range hotSong {
+					buzzer.Tone(val.tone, val.duration)
+					<-time.After(10 * time.Millisecond)
+				}
+				screen.Write(fmt.Sprintf("TOO HOT (%.2gF)", fahrenheit))
+			} else if fahrenheit < 60 {
+				for _, val := range coldSong {
+					buzzer.Tone(val.tone, val.duration)
+					<-time.After(10 * time.Millisecond)
+				}
+
+				screen.Write(fmt.Sprintf("TOO COLD (%.2gF)", fahrenheit))
+			} else {
+				screen.Write(fmt.Sprintf("%.2gF, %.2gC", fahrenheit, celsius))
+			}
+			time.Sleep(5 * time.Millisecond)
+			fmt.Printf("%.2gF, %.2gC\n", fahrenheit, celsius)
+
 			//set to params
-			newFahrenheit := math.Max(60, math.Min(80, fahrenheit))
-			screen.SetRGB(getRed(newFahrenheit), 0, getBlue(newFahrenheit))
+			screen.SetRGB(getRed(fahrenheit), 0, getBlue(fahrenheit))
 		})
 	}
 
 	robot := gobot.NewRobot("sensorBot",
 		[]gobot.Connection{board},
-		[]gobot.Device{screen, sensor},
+		[]gobot.Device{screen, buzzer, sensor},
 		work,
 	)
 
